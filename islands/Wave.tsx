48bc6850b96494ad81bea4e3a26162ef10a18e97
ayame113 <40050810+ayame113@users.noreply.github.com>
2023-05-09 01:49:29 +0900

    pref

--- islands/Wave.tsx
+++ islands/Wave.tsx
@@ -10,24 +10,6 @@ export default function Wave() {
     const ctx = canvas.getContext("2d");
 
     let diff = 0;
-
-    // const abortController = new AbortController();
-    // globalThis.addEventListener("mousemove", () => {
-    //   console.log("a");
-    //   abortController.abort();
-    //   render();
-    // }, { signal: abortController.signal });
-
-    // function render() {
-    //   if (!ctx) {
-    //     return;
-    //   }
-    //   diff++;
-    //   diff %= 360;
-    //   renderWave(ctx, diff);
-    //   animationFrameId = requestAnimationFrame(render);
-    // }
-
     let animationFrameId = requestAnimationFrame(function render() {
       if (!ctx) {
         return;
@@ -38,27 +20,8 @@ export default function Wave() {
       animationFrameId = requestAnimationFrame(render);
     });
 
-    // if (ctx) {
-    //   renderWave(ctx, diff);
-    // }
-
-    // let animationFrameId: number | undefined;
-    // requestIdleCallbackPolyfill(() => {
-    //   animationFrameId = requestAnimationFrame(function render() {
-    //     if (!ctx) {
-    //       return;
-    //     }
-    //     diff++;
-    //     diff %= 360;
-    //     renderWave(ctx, diff);
-    //     animationFrameId = requestAnimationFrame(render);
-    //   });
-    // });
-
     return () => {
-      if (animationFrameId !== undefined) {
-        cancelAnimationFrame(animationFrameId);
-      }
+      cancelAnimationFrame(animationFrameId);
     };
   }, []);
   return <canvas class="w-full h-full" width="1000" height="500" ref={ref} />;
